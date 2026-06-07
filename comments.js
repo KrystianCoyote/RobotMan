c38fc25c55c0aeb66799056093762b35f4d701e2
KrystianCoyote <krystianyahirg@gmail.com>
2026-06-07 14:32:39 -0700

    Actualizacion: API_URL configurada para produccion en Render

--- comments.js
+++ comments.js
@@ -1,4 +1,4 @@
-﻿const API_URL = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1" ? "http://localhost:5000/api" : "https://robotman-api.onrender.com/api";
+﻿const API_URL = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1" ? "http://localhost:5000/api" : "https://robotman.onrender.com/api";
 let currentUser = JSON.parse(localStorage.getItem("user"));
 let token = localStorage.getItem("token");
 
